704ad9914abe904ee789818815451993fc683f4f
Romulo Tone <romulo.tone@cevt.se>
2023-04-03 09:25:57 +0200

    Writing readme

---
+++ .storybook/etraveli.js
@@ -0,0 +1,11 @@
+// .storybook/YourTheme.js
+
+import { create } from '@storybook/theming';
+
+export default create({
+    base: 'light',
+    brandTitle: 'Code Test Documentation',
+    brandUrl: 'https://www.etraveligroup.com/',
+    brandImage: 'https://www.etraveligroup.com/wp-content/uploads/2018/11/etraveli-group-logo-2018.png',
+    brandTarget: '_self',
+});
